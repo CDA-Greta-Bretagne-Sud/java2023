18a4ede1e08c4a49aa93a9af5618fd9d1d869b55
KillianPsm <112475133+KillianPsm@users.noreply.github.com>
2023-09-15 09:18:41 +0200

    Add files via upload
    Correction mise à jour

---
+++ Bibliotheque_maj_killian/src/LivreException.java
@@ -0,0 +1,3 @@
+public class LivreException  extends Exception {
+
+}
---
+++ Bibliotheque_maj_killian/src/Util.java
@@ -0,0 +1,16 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+
+import java.util.Scanner;
+
+public class Util {
+    /**
+     * Logger static
+     */
+   protected static final Logger logger = (Logger) LogManager.getLogger(Main.class);
+    /**
+     * scanner static
+     */
+    protected static Scanner scan =
+            new Scanner(System.in);
+}
