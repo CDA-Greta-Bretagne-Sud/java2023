9c8f54f1001171313b131bf720b168c26c1a1707
Pascal LAMY <plamy@561557n.ac-rennes.fr>
2023-09-15 09:52:58 +0200

    comparator

---
+++ CompareLivre.java
@@ -0,0 +1,29 @@
+import java.util.Comparator;
+
+public  abstract class CompareLivre implements Comparator<Livre> {
+    /**
+     * Comparator pour faire un tri par titre
+     */
+    public static Comparator<Livre> TitreComparator = new Comparator<Livre>() {
+
+        @Override
+        public int compare(Livre l1, Livre l2) {
+            return l1.getTitre().compareTo(l2.getTitre());
+        }
+    };
+    public static Comparator<Livre> AuteurComparator = new Comparator<Livre>() {
+
+        @Override
+        public int compare(Livre l1, Livre l2) {
+            return l1.getAuteur().compareTo(l2.getAuteur());
+        }
+    };
+    public static Comparator<Livre> EditeurComparator = new Comparator<Livre>() {
+
+        @Override
+        public int compare(Livre l1, Livre l2) {
+            return l1.getEditeur().compareTo(l2.getEditeur());
+        }
+    };
+
+}
